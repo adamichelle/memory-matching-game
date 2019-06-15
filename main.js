952a01cade8040d134b55cc25142e3982fd0b054
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-06-15 17:22:35 +0100

    handling matched and unmatched cards

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ let cardElements = document.getElementsByClassName('game-card');
 let cardElementsArray = [...cardElements];
 let imgElements = document.getElementsByClassName('game-card-img');
 let imgElementsArray = [...imgElements];
-
+let openedCards = [];
+let matchedCards =  [];
 
 let displayCard = function () {
     this.children[0].classList.toggle('show-img');
     this.classList.toggle("open");
     this.classList.toggle("show");
     this.classList.toggle("disabled");
+    cardOpen(this);
 }
 
 function shuffle(array) {
@@ -29,15 +31,66 @@ function shuffle(array) {
 
 function startGame() {
     let shuffledImages = shuffle(imgElementsArray);
-    console.log(shuffledImages);
     for(i=0; i<shuffledImages.length; i++) {
         cardElements[i].appendChild(shuffledImages[i]);
+        cardElements[i].type = `${shuffledImages[i].alt}`;
     }
+}
+
+function cardOpen(card) {
+    openedCards.push(card);
+    let len = openedCards.length;
+    if(len === 2) {
+        if(openedCards[0].type === openedCards[1].type) {
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+}
+
+function matched() {
+    openedCards[0].classList.add("match");
+    openedCards[1].classList.add("match");
+    openedCards[0].classList.remove("show", "open");
+    openedCards[1].classList.remove("show", "open");
+    matchedCards.push(openedCards[0]);
+    matchedCards.push(openedCards[1]);
+    openedCards = [];
+}
+
+function unmatched() {
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function() {
+        openedCards[0].classList.remove("show", "open", "unmatched");
+        openedCards[1].classList.remove("show", "open", "unmatched");
+        openedCards[0].children[0].classList.remove('show-img');
+        openedCards[1].children[0].classList.remove('show-img');
+        enable();
+        openedCards = [];
+        
+    }, 1100)
+}
+
+function disable() {
+    cardElementsArray.filter((card, i, cardElementsArray) => {
+        card.classList.add('disabled');
+    })
+}
+
+function enable() {
+    cardElementsArray.filter((card, i, cardElementsArray) => {
+        card.classList.remove('disabled');
+        for(let i=0; i<matchedCards.length; i++) {
+            matchedCards[i].classList.add('disabled');
+        }
+    })
 }
 
 window.onload = startGame();
 
-
 for(let i = 0; i < cardElementsArray.length; i++) {
-    cardElementsArray[i].addEventListener("click", displayCard);
+    cardElementsArray[i].addEventListener("click", displayCard)
 }
