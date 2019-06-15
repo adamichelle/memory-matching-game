1f870a7dff8b39e86ed11c3f9fe798e2ff3cbecc
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-06-15 14:30:29 +0100

    shuffle card images on load

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let cardElements = document.getElementsByClassName('game-card');
 let cardElementsArray = [...cardElements];
+let imgElements = document.getElementsByClassName('game-card-img');
+let imgElementsArray = [...imgElements];
 
 
 let displayCard = function () {
@@ -9,6 +11,31 @@ let displayCard = function () {
     this.classList.toggle("disabled");
 }
 
+function shuffle(array) {
+    let currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+
+    while (currentIndex !==0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function startGame() {
+    let shuffledImages = shuffle(imgElementsArray);
+    console.log(shuffledImages);
+    for(i=0; i<shuffledImages.length; i++) {
+        cardElements[i].appendChild(shuffledImages[i]);
+    }
+}
+
+window.onload = startGame();
 
 
 for(let i = 0; i < cardElementsArray.length; i++) {
